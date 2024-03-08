0231a73e5ce1f4f74d203bce08ae853ca849630d
ThanadolU <dolthanadolu@gmail.com>
2024-03-08 14:14:35 +0700

    first commit

---
+++ src/main/java/ku/product/controller/ProductController.java
@@ -0,0 +1,42 @@
+package ku.product.controller;
+
+import ku.product.entity.Product;
+import ku.product.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService service;
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return service.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Product getProductId(@PathVariable int id) {
+        return service.getProductById(id);
+    }
+
+    @GetMapping("/name/{name}")
+    public Product getProductByName(@PathVariable String name) {
+        return service.getProductByName(name);
+    }
+
+    @GetMapping("/type/{type}")
+    public List<Product> getProductByType(@PathVariable String type) {
+        return service.getProductByType(type);
+    }
+
+    @PostMapping
+    public Product create(@RequestBody Product product) {
+        return service.create(product);
+    }
+}
---
+++ src/main/java/ku/product/service/ProductService.java
@@ -0,0 +1,37 @@
+package ku.product.service;
+
+import ku.product.entity.Product;
+import ku.product.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+
+    public List<Product> getAll() {
+        return repository.findAll();
+    }
+
+    public Product create(Product restaurant) {
+        Product record = repository.save(restaurant);
+        return record;
+    }
+
+    public Product getProductById(int id) {
+        return repository.findById(id).get();
+    }
+
+    public Product getProductByName(String name) {
+        return repository.findByName(name);
+    }
+
+    public List<Product> getProductByType(String type) {
+        return repository.findByType(type);
+    }
+}
